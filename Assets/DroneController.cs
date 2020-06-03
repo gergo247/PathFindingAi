e7ebaaaa0204ae56f35b3949cde1e13decc71bee
gergo247 <gergo247@gmail.com>
2020-06-03 11:23:56 +0200

    Initial commit

---
+++ Assets/DroneController.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class DroneController : MonoBehaviour
+{
+
+    public Camera camera;
+    public NavMeshAgent agent;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0)) // 0 - left mouse click
+        {
+            //creating ray
+            Ray ray = camera.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hit;
+            Debug.Log("leftclick");
+
+            if (Physics.Raycast(ray, out hit)) //if hit detected, true
+            {
+                Debug.Log("true");
+                //move
+                agent.SetDestination(hit.point);
+            }
+
+
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,2 @@
+# PathFindingAi
+ A pathfinding AI for drones
